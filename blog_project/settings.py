22031206b7ccd1534b6b004b88a15c90899384d0
Masahiro Uchiyama <uchiyama30.04@gmail.com>
2020-06-01 02:11:53 +0900

    Heroku config

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
